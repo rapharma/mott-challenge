104b51a35db4a0f4e253e10228a59b73eceec06a
raphawba <consultor.raphael@wba.com.br>
2024-06-05 16:58:02 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './pages/home/home.component';
+// import { FavoritesComponent } from './pages/favorites/favorites.component';
+
+const routes: Routes = [
+  
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(
+      (m) => m.PagesModule
+    )
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { metaReducers } from './store/app.state';
+import { appReducer } from './store/reducers';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({app: appReducer}, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+  ],
+  providers: [
+    provideAnimationsAsync(),
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
